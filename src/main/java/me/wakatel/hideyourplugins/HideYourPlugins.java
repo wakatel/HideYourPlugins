5b5aa37666d92113ccd5d0614892809d27c7d85f
wakatel <84437677+wakatel@users.noreply.github.com>
2022-08-29 04:39:12 +0700

    1.3
    im a dumbass

--- src/main/java/me/wakatel/hideyourplugins/HideYourPlugins.java
+++ src/main/java/me/wakatel/hideyourplugins/HideYourPlugins.java
@@ -8,33 +8,41 @@ import me.wakatel.hideyourplugins.Events.SyntaxBlocker;
 import me.wakatel.hideyourplugins.Events.VersionSpoofer;
 import me.wakatel.hideyourplugins.Packets.TabComplete;
 import org.bstats.bukkit.Metrics;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.logging.Logger;
 
 public final class HideYourPlugins extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Logger logger = Logger.getLogger("HYP");
         int pluginId = 16276;
         Metrics metrics = new Metrics(this, pluginId);
-        getConfig().options().copyDefaults();
-        saveConfig();
-        getCommand("hypreload").setExecutor(new ReloadCommand());
+        if (!(new File(getDataFolder(), "config.yml")).exists()) {
+            getConfig().options().copyDefaults(true);
+            saveDefaultConfig();
+            getCommand("hypreload").setExecutor(new ReloadCommand());
+        }
         getServer().getPluginManager().registerEvents(new PluginHider(), this);
         getServer().getPluginManager().registerEvents(new SyntaxBlocker(), this);
         getServer().getPluginManager().registerEvents(new VersionSpoofer(), this);
         if (getServer().getPluginManager().getPlugin("ProtocolLib") != null) {
-            getLogger().info("ProtocolLib detected.");
-            getLogger().info("Registering packet listener...");
+            logger.info("ProtocolLib detected.");
+            logger.info("Registering packet listener...");
             ProtocolLibrary.getProtocolManager().addPacketListener(new TabComplete(this, new PacketType[] { PacketType.Play.Client.TAB_COMPLETE }));
-            getLogger().info("Register packet complete!");
-            getLogger().info(ChatColor.GREEN + "Thank you for using this plugin!");
-            getLogger().info(ChatColor.GREEN + "Please leave a rating :)");
-            getLogger().info(ChatColor.GREEN + "https://www.spigotmc.org/resources/hideyourplugins-advanced-plugins-hider.104859/");
-            getLogger().info(ChatColor.GREEN + "by. wakatel");
+            logger.info("Register packet complete!");
+            logger.info("+-----------------------------+");
+            logger.info("HideYourPlugins 1.2");
+            logger.info("Thank you for using this plugin!");
+            logger.info("Please leave a rating :)");
+            logger.info("https://www.spigotmc.org/resources/hideyourplugins-advanced-plugins-hider.104859/");
+            logger.info("by. wakatel");
+            logger.info("+-----------------------------+");
         }else {
-            getLogger().warning("Could not find ProtocolLib! This plugin is required.");
-            getLogger().warning("Download ProtocolLib here https://github.com/dmulloy2/ProtocolLib/releases");
+            logger.warning("Could not find ProtocolLib! This plugin is required.");
+            logger.warning("Download ProtocolLib here https://github.com/dmulloy2/ProtocolLib/releases");
         }
     }
 }
